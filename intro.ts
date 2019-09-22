0c793272605a0a046725368f25be65844b604438
Ercan-home <ercanerdogan@gmail.com>
2019-09-22 06:22:51 +0300

    any type declaration

--- intro.ts
+++ intro.ts
@@ -1,9 +1,9 @@
 function selamVer(isim : string)
 {
-    return "Merhaba " + isim
+    return "Hi " + isim
 }
 
-let mesaj = selamVer("Ercan Erdoğan")
+let mesaj = selamVer("Ercan")
 
 console.log(mesaj)
 
@@ -12,3 +12,13 @@ enum Renkler {Kırmızı=1, Siyah, Mavi}
 let renk : Renkler = Renkler.Kırmızı
 
 console.log(renk)
+
+
+//deger variable could set any type of value
+let deger : any 
+deger=2
+deger="hello"
+
+deger={adi: "Ercan", mobileNumber : 123456 }
+
+
